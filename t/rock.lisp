f7f2dca444e971238b26c09802143cde510eedc3
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-25 17:57:39 -0200

    Expand tests

--- t/rock.lisp
+++ t/rock.lisp
@@ -17,12 +17,23 @@
     (rock:defenv :rock-test
       :assets ((:jquery :2.1.0)
                (:jquery :1.9.1)
-               (:composer.js :1.0))
-      :bundles ((;; Old jquery + composer
+               (:composer.js :1.0)
+               (:bootstrap :3.2.0))
+      :bundles ((;; Old jQuery + Composer.js
+                 :js
+                 :assets ((:jquery :1.9.1)
+                          (:composer.js :1.0))
+                 :destination #p"js/old.js")
+                (;; New jQuery + Composer.js + Bootstrap JS
                  :js
                  :assets ((:jquery :2.1.0)
-                          (:composer.js :1.0))
-                 :destination #p"js/old.js")))))
+                          (:composer.js :1.0)
+                          (:bootstrap :3.2.0))
+                 :destination #p"js/new.js")
+                (;; Bootstrap CSS
+                 :css
+                 :assets ((:bootstrap :3.2.0))
+                 :destination #p"css/style.css")))))
 
 (defun destroy-assets-directory ()
   (when (cl-fad:directory-exists-p +assets-directory+)
@@ -47,7 +58,19 @@
     (asset-relative-path #p"composer.js-1.0/composer.min.js")))
   (is-true
    (probe-file
-    (asset-relative-path #p"build/js/old.js"))))
+    (asset-relative-path #p"bootstrap-3.2.0/js/bootstrap.min.js")))
+  (is-true
+   (probe-file
+    (asset-relative-path #p"bootstrap-3.2.0/css/bootstrap.min.css")))
+  (is-true
+   (probe-file
+    (asset-relative-path #p"build/js/old.js")))
+  (is-true
+   (probe-file
+    (asset-relative-path #p"build/js/new.js")))
+  (is-true
+   (probe-file
+    (asset-relative-path #p"build/css/style.css"))))
 
 (test cleanup
   (finishes
